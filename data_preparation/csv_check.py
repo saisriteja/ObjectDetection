a532c19947550c0d086bc78b97b447ea6982a63b
saisriteja <kuppasaisriteja@gmail.com>
2024-03-14 23:28:42 +0530

    rough check

--- data_preparation/csv_check.py
+++ data_preparation/csv_check.py
@@ -1,14 +1,24 @@
 import pandas as pd
 
-data = pd.read_csv('csv_info/voc_annotation.csv')
+data = pd.read_csv('/data/tmp_teja/datacv/final/ObjectDetection/clipNN_output/clipNN_pruned.csv')
 
 import cv2
 
 img  = cv2.imread(data['img_path'][0])
+
+print(img.shape)
+
 x_min = data['x_min'][0]
 y_min = data['y_min'][0]
 width = data['width'][0]
 height = data['height'][0]
+
+x_min = int(x_min)
+y_min = int(y_min)
+width = int(width)
+height = int(height)
+
+print(x_min, y_min, width, height)
 
 # draw a box around the object
 cv2.rectangle(img, (x_min, y_min), (x_min+width, y_min+height), (0, 255, 0), 2)
