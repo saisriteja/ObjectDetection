8ef0cb6dfde034d6bc3a0049806e736fc08c5125
saisriteja <kuppasaisriteja@gmail.com>
2024-03-14 23:41:02 +0530

    weightage based sampling

--- data_pruning/clipNN_pruning.py
+++ data_pruning/clipNN_pruning.py
@@ -7,10 +7,8 @@ from logzero import logger
 import fiftyone.brain as fob
 
 def get_pruned_dataset(csv_paths, 
-                       output_path, 
                        total_images,
                        visdrone_img_paths,
-                       c_num,
                        n_neighbors= 5,
                        result_dir = 'clipNN_output'):
     
@@ -137,16 +135,52 @@ def get_pruned_dataset(csv_paths,
     indices = np.unique(indices)
     img_names=unq_img_paths[indices]
 
-    img_names = img_names[:total_images]
 
+    # img_names = img_names[:total_images]
+
+
+    # =========================================> Pruning the dataset using weightage <========================================
+
+    # split the img_path and get the last second one as the label in the dataframe
+    data['label'] = data['img_path'].apply(lambda x: x.split('/')[-2])
+
+    # get the ratio of labels in the total dataset
+    label_ratio = data['label'].value_counts(normalize=True)
+
+    # get the median of the label ratio
+    median_label_ratio = label_ratio.median()
+
+    # if any value is greater than the median, assign it to the median
+    label_ratio[label_ratio > median_label_ratio] = median_label_ratio
+
+    # multiply the label ratio with the total images to get the number of images per label
+    label_ratio = (label_ratio * total_images).astype(int)
+
+    # if the sum of the label ratio is less than the total images, assign the remaining images to the label with the highest count
+    if label_ratio.sum() < total_images:
+        label_ratio[label_ratio.idxmax()] += (total_images - label_ratio.sum())
+
+    # print(label_ratio)
+    img_names = []
+    for label, count in label_ratio.items():
+        df = data[data['label'] == label]
+        # get the unq names
+        df_unq = df['img_path'].unique()
+        # get the count of images
+        count = min(count, len(df_unq))
+        img_names.extend(df_unq[:count])
+
+    
+
+    # =====================================================> Saving the CSV file <================================================
 
     final_df = data[data['img_path'].isin(img_names)]
 
     # check if final unq images are equal to the total images
     if len(final_df['img_path'].unique()) != total_images:
-        logger.warn("The number of images are not equal to the total images")
-        logger.warn("Images in final df: " + str( len(final_df['img_path'].unique())))
-        logger.warn("Total images: " + str(total_images))
+        logger.warning("The number of images are not equal to the total images")
+        logger.warning("Images in final df: " + str( len(final_df['img_path'].unique())))
+        logger.warning("Total images: " + str(total_images))
         # logger.error(len(final_df['img_path'].unique()), total_images)
         pass
 
@@ -169,14 +203,8 @@ if __name__ == '__main__':
     visdrone_img_paths = glob(visdrone_paths)
     visdrone_img_paths.sort()
 
-
-    # no of clusters
-    c_num = len(glob('/data/tmp_teja/datacv/final/visdrone/VisDrone2019-VID-train/sequences/*'))
-
-
-    output_path = 'csv_info'
     total_images = 10
-    nearest_neighbours = 10
-    get_pruned_dataset(csv_paths, output_path, total_images,
-                          visdrone_img_paths, c_num, n_neighbors = nearest_neighbours)
+    nearest_neighbours = 50
+    get_pruned_dataset(csv_paths, total_images,
+                          visdrone_img_paths,n_neighbors = nearest_neighbours)
 
